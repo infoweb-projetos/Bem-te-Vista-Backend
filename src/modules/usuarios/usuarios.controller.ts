06fe3f917a6f76924fc7919481ecbb5668c9c4a3
JordsonZ <jordsongalvao@gmail.com>
2024-08-19 09:37:12 -0300

    crud user service

--- src/modules/usuarios/usuarios.controller.ts
+++ src/modules/usuarios/usuarios.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { UsuarioDTO } from './usuario.dto';
 
@@ -9,5 +9,20 @@ export class UsuariosController {
   @Post()
   async create(@Body() data: UsuarioDTO){
     return this.usuariosService.create(data);
+  }
+
+  @Get()
+  async findAll(){
+    return this.usuariosService.findAll();
+  }
+
+  @Put(":id")
+  async update(@Param("id") id: string, @Body() data: UsuarioDTO){
+    return this.usuariosService.update(id, data);
+  }
+
+  @Delete(":id")
+  async delete(@Param('id') id: string){
+    return this.usuariosService.delete(id);
   }
 }
--- src/modules/usuarios/usuarios.service.ts
+++ src/modules/usuarios/usuarios.service.ts
@@ -23,4 +23,43 @@ export class UsuariosService {
     return user;
 }
 
+    async findAll(){
+        return this.prisma.user.findMany();
+    }
+
+    async update(id: string, data: UsuarioDTO){
+        const userExists = await this.prisma.user.findUnique({
+            where: {
+                id,
+            },
+        })
+
+        if(userExists) {
+            throw new Error('Esse usuário não existe');
+        }
+        return await this.prisma.user.update({
+            data,
+            where: {
+                id,
+            }
+        })
+    }
+    
+    async delete(id: string){
+        const userExists = await this.prisma.user.findUnique({
+            where: {
+                id,
+            },
+        });
+
+        if(userExists) {
+            throw new Error('Esse usuário não existe');
+        }
+
+        return await this.prisma.user.delete({
+            where: {
+                id,
+            },
+        });
+    }
 }
